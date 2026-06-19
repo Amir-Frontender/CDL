8476d246a3976b83dfbc596707f6f75ac877352b
Amir Ibragimov <amir.ibragimov1991@gmail.com>
2026-06-19 16:07:30 +0500

    Revert to aa42baa state

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/ru/",
+        permanent: false,
+      },
+    ];
+  },
   async headers() {
     return [
       {
